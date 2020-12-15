62800a54837034d2b07dbd296504ced86f491640
Flowtter <naexys.naexys@gmail.com>
2020-12-15 23:30:51 +0100

    Main

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -16,17 +16,3 @@ void bintree_free(bt *bintree) {
         bintree_free(bintree->right);
     free(bintree);
 }
-
-// size_t size(bt *bintree) {
-//    size_t counter = 0;
-//    //queue q;
-//
-//    return counter;
-//}
-
-// int main() {
-//    //bt init = {.key = 0, .left = NULL, .right = NULL};
-//    //init.left = &(bt){.key = 1, .left = NULL, .right = NULL};
-//    //init.right = &(bt){.key = 2, .left = NULL, .right = NULL};
-//    return 0;
-//}
